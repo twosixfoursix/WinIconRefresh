58f1dde9255545681f09aae12d22d19978388536
TwoSixFourSix <156486165+twosixfoursix@users.noreply.github.com>
2024-01-22 08:50:14 -0400

    Create icref.cxx

---
+++ icref/icref.cxx
@@ -0,0 +1,10 @@
+#include <shlobj_core.h>
+
+// cl /nologo /EHsc /c icref.cxx
+// link /nologo /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup icref.obj shell32.lib /OUT:icref.exe
+
+int main()
+{
+	SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, NULL, NULL);
+	return 0;
+}
