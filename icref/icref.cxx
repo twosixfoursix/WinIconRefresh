c992a8894e5af0654849bbbd5fe7e97aa40ef7e1
TwoSixFourSix <156486165+twosixfoursix@users.noreply.github.com>
2025-08-09 03:14:04 +0000

    Update icref.cxx
    This code is obsolete.

--- icref/icref.cxx
+++ icref/icref.cxx
@@ -1,10 +1,12 @@
-#include <shlobj_core.h>
+// OBSOLETE: DO NOT USE
+
+// #include <shlobj_core.h>
 
 // cl /nologo /EHsc /c icref.cxx
 // link /nologo /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup icref.obj shell32.lib /OUT:icref.exe
 
-int main()
-{
-	SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, NULL, NULL);
-	return 0;
-}
+// int main()
+// {
+// 	SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, NULL, NULL);
+// 	return 0;
+// }
